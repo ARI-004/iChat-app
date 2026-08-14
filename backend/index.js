a2f6b1e8b8dc3c42c9895766de5bec268d5b1ea9
ARIJIT ATTA <attaarijit4002@gmail.com>
2026-08-15 03:15:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,39 @@
-const io = require('socket.io')(8000,{
+const io = require('socket.io')(process.env.PORT || 8000, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
     }
-})
+});
 
-const users = {}
-
-
+const users = {};
 
 io.on('connection', socket => {
+
+    console.log("Connected:", socket.id);
+
     socket.on('new-user-joined', name => {
-        //console.log("New user", name)
-        users[socket.id] = name
-        socket.broadcast.emit('user-joined', name)
+        console.log("New user:", name);
+
+        users[socket.id] = name;
+
+        socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+        console.log("Message:", message);
+        console.log("From:", users[socket.id]);
+
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id]
+        });
     });
 
-    socket.on('disconnect', message => {
-        socket.broadcast.emit('left', users[socket.id])
-        delete users[socket.id]
+    socket.on('disconnect', () => {
+        console.log("Disconnected:", socket.id);
+
+        socket.broadcast.emit('left', users[socket.id]);
+
+        delete users[socket.id];
     });
 });
